ee35057b770d3e2e1302fbff65846821b2deaf46
Kurpol <lonxerk3@gmail.com>
2024-06-23 19:05:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,88 @@
+﻿using ToDoList;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        TodoList toDoList = new TodoList();
+        string command = "";
+
+        while (command != "exit")
+        {
+            Console.WriteLine("\nВыберите действие: add, update, remove, list, dictionary, sortdate, sortpriority, exit");
+            command = Console.ReadLine();
+
+            switch (command)
+            {
+                case "add":
+                    Console.Write("Введите задачу: ");
+                    string task = Console.ReadLine();
+                    Console.Write("Введите приоритет (целое число): ");
+                    int priority = int.Parse(Console.ReadLine());
+                    toDoList.AddNewTask(priority, task);
+                    Console.WriteLine("Задача добавлена.");
+                    break;
+
+                case "update":
+                    Console.Write("Введите ID задачи для обновления: ");
+                    int updateId = int.Parse(Console.ReadLine());
+                    Console.Write("Введите новую задачу: ");
+                    string newTask = Console.ReadLine();
+                    Console.Write("Введите новый приоритет (целое число): ");
+                    int newPriority = int.Parse(Console.ReadLine());
+                    toDoList.UpdateTask(updateId, newTask, newPriority);
+                    Console.WriteLine("Задача обновлена.");
+                    break;
+
+                case "remove":
+                    Console.Write("Введите ID задачи для удаления: ");
+                    int removeId = int.Parse(Console.ReadLine());
+                    toDoList.RemoveTask(removeId);
+                    Console.WriteLine("Задача удалена.");
+                    break;
+
+                case "list":
+                    Console.WriteLine("Все задачи List:");
+                    foreach (var item in toDoList.GetAllList())
+                    {
+                        Console.WriteLine($"{item.Id}. {item.Task} - Приоритет: {item.Priority} - Добавлено: {item.Date}");
+                    }
+                    break;
+
+                case "dictionary":
+                    Console.WriteLine("Все задачи Dictionary:");
+                    foreach (var temp in toDoList.GetAllDictionary())
+                    {
+                        var item = temp.Value;
+                        Console.WriteLine($"{item.Id}. {item.Task} - Приоритет: {item.Priority} - Добавлено: {item.Date}");
+                    }
+                    break;
+
+                case "sortdate":
+                    Console.WriteLine("Задачи, отсортированные по дате добавления:");
+                    foreach (var item in toDoList.SortByDate())
+                    {
+                        Console.WriteLine($"{item.Id}. {item.Task} - Приоритет: {item.Priority} - Добавлено: {item.Date}");
+                    }
+                    break;
+
+                case "sortpriority":
+                    Console.WriteLine("Задачи, отсортированные по приоритету:");
+                    foreach (var item in toDoList.SortByPriority())
+                    {
+                        Console.WriteLine($"{item.Id}. {item.Task} - Приоритет: {item.Priority} - Добавлено: {item.Date}");
+                    }
+                    break;
+
+                case "exit":
+                    Console.WriteLine("Выход из программы.");
+                    break;
+
+                default:
+                    Console.WriteLine("Неверная команда. Попробуйте снова.");
+                    break;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ToDoList.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoItem;
+
+
+namespace ToDoList
+{
+    internal class TodoList
+    {
+        private List<TodoListItem> list;
+        private Dictionary<int, TodoListItem> dictionary;
+
+        public TodoList()
+        {
+            list = new List<TodoListItem>();
+            dictionary = new Dictionary<int, TodoListItem>();
+        }
+
+        public void AddNewTask(int priority, string tasktext) 
+        {
+            TodoListItem newItem = new TodoListItem {
+
+                Id = list.Count > 0 ? list.Max(x => x.Id) + 1 : 1,
+                Task = tasktext,
+                Priority = priority,
+                Date = DateTime.Now,
+            };
+            list.Add(newItem);
+            dictionary[newItem.Id] = newItem;
+        }
+
+        public void UpdateTask(int id, string newtasktext, int newpriority)
+        {
+            if (dictionary.ContainsKey(id))
+            {
+                TodoListItem newItem = dictionary[id];
+                newItem.Task = newtasktext;
+                newItem.Priority = newpriority; 
+
+                var index = list.FindIndex(x => x.Id == id);
+                if (index != -1)
+                {
+                    list[index] = newItem;
+                }
+            }
+        }
+
+        public void RemoveTask(int id) 
+        {
+            if (dictionary.ContainsKey(id))
+            {
+                dictionary.Remove(id);
+                list.RemoveAll(x => x.Id == id);
+            }
+        }
+
+        public List<TodoListItem> GetAllList()
+        { return list; }
+
+        public Dictionary<int, TodoListItem> GetAllDictionary()
+        { return dictionary; }
+
+
+        public List<TodoListItem> SortByDate()
+        {
+            return list.OrderBy(x => x.Date).ToList();
+        }
+
+        // Сортировка по приоритетам
+        public List<TodoListItem> SortByPriority()
+        {
+            return list.OrderBy(x => x.Priority).ToList();
+        }
+
+    }
+}
